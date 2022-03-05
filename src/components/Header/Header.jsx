d8ff42be10d5b0044fa8442afb71d9f602a6a12e
Cocoflosbach <84343628+Cocoflosbach@users.noreply.github.com>
2022-03-05 16:20:13 +0100

    Create and style header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Header.css";
+import CTA from "./CTA";
+import IMG from "../../assets/header.jpg";
+import Socials from "./Socials";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container header__container" id="home">
+        <h5>Hello I'm</h5>
+        <h1>Tolulope Flosbach</h1>
+        <h5 className="text-light"> Fullstack Developer</h5>
+        <CTA />
+        <Socials />
+
+        <div className="me">
+          <img src={IMG} alt="" />
+        </div>
+
+        <a href="#contact" className="scroll__down">
+          Scroll down
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
