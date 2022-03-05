cca8194e173aa94585ae1810bdf5889a0d820a98
Cocoflosbach <84343628+Cocoflosbach@users.noreply.github.com>
2022-03-05 16:18:44 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Header from "./components/Header/Header";
+import Nav from "./components/Nav/Nav";
+import AboutMe from "./components/AboutMe/AboutMe";
+import Experience from "./components/Experience/Experience";
+import Portfolio from "./components/Portfolio/Portfolio";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <AboutMe />
+      <Experience />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
