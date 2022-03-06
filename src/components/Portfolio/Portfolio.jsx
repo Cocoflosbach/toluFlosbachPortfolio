0f9d9fbc870e10059bef7b56892e7fe5d8653bd7
Cocoflosbach <84343628+Cocoflosbach@users.noreply.github.com>
2022-03-06 14:22:31 +0100

    Update Portfolio styling

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -47,8 +47,8 @@ const Portfolio = () => {
 
   return (
     <section id="portfolio">
-      <h5>My Recent Works</h5>
-      <h2>portfolio</h2>
+      <h5>My Recent Projects</h5>
+      <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
         {projects.map(({ id, image, title, github, demo, description }) => {
