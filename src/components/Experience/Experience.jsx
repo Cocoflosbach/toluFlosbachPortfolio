e13d18180370683360d0112fd39b3e42318d5e93
Cocoflosbach <84343628+Cocoflosbach@users.noreply.github.com>
2022-03-06 14:21:09 +0100

    Add experience rating scale

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./Experience.css";
 import { BsPatchCheckFill } from "react-icons/bs";
+import { RiStarSFill } from "react-icons/ri";
+import { BsFillSquareFill } from "react-icons/bs";
+import { BsSquare } from "react-icons/bs";
 
 const Experience = () => {
   return (
@@ -16,56 +19,144 @@ const Experience = () => {
               <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>HTML</h4>
-                <small className="text-light">Experienced</small>
+                <small className="text-light experience__rating">
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsSquare className="experience__rating-item" />
+                </small>
               </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>CSS</h4>
-                <small className="text-light">Intermediate</small>
+                <small className="text-light experience__rating">
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsSquare className="experience__rating-item" />
+                  <BsSquare className="experience__rating-item" />
+                  <BsSquare className="experience__rating-item" />
+                  <BsSquare className="experience__rating-item" />
+                </small>
               </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>Javascript</h4>
-                <small className="text-light">Experienced</small>
+                <small className="text-light experience__rating">
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsSquare className="experience__rating-item" />
+                  <BsSquare className="experience__rating-item" />
+                </small>
               </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>React</h4>
-                <small className="text-light">Experienced</small>
+                <small className="text-light experience__rating">
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsSquare className="experience__rating-item" />
+                  <BsSquare className="experience__rating-item" />
+                </small>
               </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>Angular</h4>
-                <small className="text-light">Intermediate</small>
+                <small className="text-light experience__rating">
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsSquare className="experience__rating-item" />
+                  <BsSquare className="experience__rating-item" />
+                  <BsSquare className="experience__rating-item" />
+                  <BsSquare className="experience__rating-item" />
+                </small>
               </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>Bootstrap</h4>
-                <small className="text-light">Experienced</small>
+                <small className="text-light experience__rating">
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsSquare className="experience__rating-item" />
+                  <BsSquare className="experience__rating-item" />
+                </small>
               </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>Tailwind</h4>
-                <small className="text-light">Intermediate</small>
+                <small className="text-light experience__rating">
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsSquare className="experience__rating-item" />
+                  <BsSquare className="experience__rating-item" />
+                  <BsSquare className="experience__rating-item" />
+                  <BsSquare className="experience__rating-item" />
+                </small>
               </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>React Native</h4>
-                <small className="text-light">Intermediate</small>
+                <small className="text-light experience__rating">
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsSquare className="experience__rating-item" />
+                  <BsSquare className="experience__rating-item" />
+                  <BsSquare className="experience__rating-item" />
+                  <BsSquare className="experience__rating-item" />
+                </small>
               </div>
             </article>
           </div>
@@ -79,28 +170,72 @@ const Experience = () => {
               <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>Node.JS</h4>
-                <small className="text-light">Experienced</small>
+                <small className="text-light experience__rating">
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsSquare className="experience__rating-item" />
+                  <BsSquare className="experience__rating-item" />
+                </small>
               </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>MongoDB</h4>
-                <small className="text-light">Experienced</small>
+                <small className="text-light experience__rating">
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsSquare className="experience__rating-item" />
+                </small>
               </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>Firestore</h4>
-                <small className="text-light">Intermediate</small>
+                <small className="text-light experience__rating">
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsSquare className="experience__rating-item" />
+                  <BsSquare className="experience__rating-item" />
+                  <BsSquare className="experience__rating-item" />
+                  <BsSquare className="experience__rating-item" />
+                </small>
               </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>Python</h4>
-                <small className="text-light">Beginner</small>
+                <small className="text-light experience__rating">
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsFillSquareFill className="experience__rating-item" />
+                  <BsSquare className="experience__rating-item" />
+                  <BsSquare className="experience__rating-item" />
+                  <BsSquare className="experience__rating-item" />
+                  <BsSquare className="experience__rating-item" />
+                  <BsSquare className="experience__rating-item" />
+                  <BsSquare className="experience__rating-item" />
+                </small>
               </div>
             </article>
           </div>
