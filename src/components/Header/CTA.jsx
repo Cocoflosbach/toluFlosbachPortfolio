ecae9afb196a85c7023742fc8535908169633dcd
Cocoflosbach <84343628+Cocoflosbach@users.noreply.github.com>
2022-03-06 14:21:49 +0100

    Update CTA.jsx

--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -5,10 +5,10 @@ const CTA = () => {
   return (
     <div className="cta">
       <a href={CV} download className="btn">
-        Download CV
+        My Resume
       </a>
       <a href="#contact" className="btn btn-primary">
-        Let's Talk
+        Let's Chat
       </a>
     </div>
   );
